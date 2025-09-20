eadeb082e6c1c560c765cbaa98c993ef3b7cde08
Arthur <arthurlazarogaldino@gmail.com>
2025-09-20 15:51:29 -0300

    Consertando, tinha escrito controller.

--- Backend/Models/Medicamentos/MedicamentosModel.cs
+++ Backend/Models/Medicamentos/MedicamentosModel.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace Backend.Models.Medicamentos
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MedicamentosController : ControllerBase
+    
+    public class MedicamentosModel
     {
         public enum PrioridadeEnum
         {
@@ -17,33 +17,18 @@ namespace Backend.Models.Medicamentos
 
 
         public PrioridadeEnum Prioridade { get; set; }
-
-
-
-
-        public String DescricaoMedicamentos { get; set; }
-
-
-
+        public required String DescricaoMedicamentos { get; set; }
         public DateTime DataEntrega { get; set; }
-
-
-
-
         public string NotaFiscal { get; set; }
-
         public string NomeComercial { get; set; }
         public string HorV { get; set; }// Esse perguntar pra eles oq significa para melhorar o nome
         public int ConsumoMensal { get; set; }
-
         public int ConsumoAnual { get; set; }
-
         public DateTime Validade { get; set; }
-
+        [Key]                      
+        public int CodigoId {  get; set; }
         public int EstoqueDisponivel { get; set; }
-
         public int EntradaEstoque {  get; set; }
-
         public int SaidaTotalEstoque {  get; set; }
 
 
