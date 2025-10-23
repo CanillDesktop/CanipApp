397fb0e96587aea3d2964e4daa3f7b6f32d15439
Mateus Tomaz <mstomaz11@gmail.com>
2025-10-23 18:44:16 -0300

    adicionando um sistema de tabs na pagina de produtos: falta replicar para as outras e terminar os detalhes

---
+++ Frontend/Models/TabItemModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Frontend.Models;
+public class TabItemModel : ObservableObject
+{
+    private string _name = string.Empty;
+    private bool _isVisible;
+
+    public TabItemModel(string name, bool visible = false)
+    {
+        Name = name;
+        IsVisible = visible;
+    }
+    public string Name
+    {
+        get => _name;
+        set => SetProperty(ref _name, value);
+    }
+    public bool IsVisible
+    {
+        get => _isVisible;
+        set => SetProperty(ref _isVisible, value);
+    }
+}
--- Frontend/ViewModels/ProdutosViewModel.cs
+++ Frontend/ViewModels/ProdutosViewModel.cs
@@ -1,14 +1,23 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Frontend.Models;
+using Frontend.ViewModels.Interfaces;
 using Shared.DTOs;
 using System.Collections.ObjectModel;
 using System.Net.Http.Json;
 
 namespace Frontend.ViewModels
 {
-    public partial class ProdutosViewModel : ObservableObject
+    public partial class ProdutosViewModel : ObservableObject, ILoadableViewModel, ITabableViewModel
     {
         private readonly HttpClient _http;
+        private bool _hasTabs;
+        private readonly ObservableCollection<TabItemModel> _tabsShowing =
+        [
+            new TabItemModel("Produtos", true),
+            new TabItemModel("Cadastrar")
+        ];
+        private string _activeTab = "Produtos";
 
         public ObservableCollection<ProdutosDTO> Produtos { get; } = [];
 
@@ -22,13 +31,42 @@ namespace Frontend.ViewModels
                 SetProperty(ref _carregando, value);
             }
         }
+        public bool HasTabs
+        {
+            get => _hasTabs;
+            set
+            {
+                SetProperty(ref _hasTabs, value);
+            }
+        }
+
+        public ObservableCollection<TabItemModel> TabsShowing
+        {
+            get => _tabsShowing;
+        }
+
+        public string ActiveTab
+        {
+            get => _activeTab;
+            set
+            {
+                if(SetProperty(ref _activeTab, value))
+                {
+                    OnTabChanged?.Invoke();
+                }
+            }
+        }
+
+        public Action? OnTabChanged { get; set; }
 
         public IAsyncRelayCommand CarregarProdutosCommand;
+        public IRelayCommand CadastrarProdutosCommand;
 
         public ProdutosViewModel(IHttpClientFactory httpClientFactory)
         {
             _http = httpClientFactory.CreateClient("ApiClient");
             CarregarProdutosCommand = new AsyncRelayCommand(CarregarProdutosAsync);
+            CadastrarProdutosCommand = new RelayCommand(CadastrarProdutosAsync);
         }
 
         private async Task CarregarProdutosAsync()
@@ -51,6 +89,18 @@ namespace Frontend.ViewModels
             {
                 Carregando = false;
             }
+        }
+
+        public async Task OnLoadedAsync()
+        {
+            await CarregarProdutosAsync();
+        }
+
+        private void CadastrarProdutosAsync()
+        {
+            HasTabs = true;
+            TabsShowing.First(t => t.Name == "Cadastrar").IsVisible = true;
+            ActiveTab = "Cadastrar";
         }
     }
 }
